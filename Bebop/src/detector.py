4aee6f94e153f0a95d0012973cfc35f815781ec2
samuellimabraz <samuellimabraz@gmail.com>
2023-06-28 03:11:59 -0300

    feat: Modularizando as detecções

--- Bebop/src/detector.py
+++ Bebop/src/detector.py
@@ -6,163 +6,42 @@ from cv_bridge import CvBridge, CvBridgeError
 
 import cv2
 
-from cvzone.FaceDetectionModule import FaceDetector
-
-import mediapipe as mp
+from HandModule import MyHandDetector
+from PoseModule import MyPoseDetector
 
 import numpy as np
 
 from utils import *
 
 
-class Detector:
-    def __init__(self) -> None:
+class Detector(MyHandDetector, MyPoseDetector):
+    def __init__(
+        self,
+        mode=False,
+        maxHands=2,
+        minHandDetectionCon=0.5,
+        minHandTrackCon=0.5,
+        minFaceDetectionCon=0.5,
+        minPoseDetectionCon=0.5,
+        minPoseTrackCon=0.5,
+    ):
+        MyHandDetector.__init__(
+            self,
+            mode,
+            maxHands,
+            minHandDetectionCon,
+            minHandTrackCon,
+            minFaceDetectionCon,
+        )
+        MyPoseDetector.__init__(self, mode, True, minPoseDetectionCon, minPoseTrackCon)
+
         rospy.init_node("detector_node", anonymous=True)
-
         self.image_topic = rospy.get_param("~image_topic", "webcam_image")
-
-        self.handDetector = MyHandDetector(
-            maxHands=1, detectionCon=0.8, minTrackCon=0.7
-        )
-        self.faceDetector = FaceDetector(minDetectionCon=0.8)
-
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_drawing_styles = mp.solutions.drawing_styles
-        self.mp_pose = mp.solutions.pose
-        self.pose = self.mp_pose.Pose(
-            min_detection_confidence=0.8, min_tracking_confidence=0.8
-        )
 
         self.hand_detect_img = np.zeros((480, 640, 3))
         self.pose_detect_img = np.zeros((480, 640, 3))
 
         self.bridge = CvBridge()
-
-    def hand_detect(self, img):
-        """
-        Realiza a detecção da mão em uma área próximo ao rosto,
-        retornando o evento interpretado pelo gesto identificado
-        """
-        if self.image_topic == "bebop":
-            try:
-                cv_img = self.bridge.compressed_imgmsg_to_cv2(img, "bgr8")
-            except CvBridgeError as e:
-                print(e)
-        else:
-            cv_img = img.copy()
-
-        # Detecta o rosto na imagem
-        img, bboxs = self.faceDetector.findFaces(cv_img)
-
-        event = "None"
-
-        # Se há rosto, cria uma area de reconhecimento de gestos
-        if bboxs:
-            # Cria área de reconhecimento
-            xb, yb, wb, hb = bboxs[0]["bbox"]
-            w = abs(wb + 40)
-            h = abs(hb + 60)
-            x = abs(xb - w - 40)
-            y = abs(yb)
-            drawRectangleEdges(cv_img, x, y, w, h, 20)
-
-            detect = cv_img[y : (y + h), x : (x + w)]
-
-            # Detecta a mão e identifica o gesto pela posição dos dedos
-            hands, detect = self.handDetector.findHands(detect)
-
-            if hands:
-                hand = hands[0]
-                # Detecta os dedos levantados ou não
-                fingers = self.handDetector.fingersUp(hand)
-
-                # Cria os eventos para cada gesto
-                if fingers == [0, 1, 0, 0, 0]:
-                    event = "UP"
-                elif fingers == [0, 1, 1, 0, 0]:
-                    event = "DOWN"
-                elif fingers == [1, 1, 1, 1, 1]:
-                    event = "WAIT"
-                elif fingers == [1, 0, 0, 0, 0]:
-                    event = "LEFT"
-                elif fingers == [0, 0, 0, 0, 1]:
-                    event = "RIGHT"
-                elif fingers == [0, 0, 0, 0, 0]:
-                    event = "FRONT"
-                elif fingers == [0, 1, 0, 0, 1]:
-                    event = "FLIP"
-
-                cv2.putText(
-                    cv_img,
-                    event,
-                    (x, y),
-                    cv2.FONT_HERSHEY_PLAIN,
-                    2,
-                    (255, 0, 255),
-                    2,
-                )
-
-        rospy.loginfo(f"Event: {event}")
-
-        # cv2.imshow("Hand", cv_img)
-        # cv2.waitKey(1)
-
-        self.hand_detect_img = cv_img
-    
-
-    def people_detect(self, img):
-
-        crop_img = cropImage(img, 0.0, 0.47)
-        crop_img = np.ascontiguousarray(img)
-
-        # Para melhorar o desempenho, opcional
-        crop_img.flags.writeable = False
-        # Processa a imagem com o modelo do mediapipe
-        results = self.pose.process(crop_img)
-
-        area, center, dist = 0, 0, 0
-
-        if results.pose_landmarks:
-            crop_img.flags.writeable = True
-
-            # Desenho dos pose landmarks no frame
-            self.mp_drawing.draw_landmarks(
-                img,
-                results.pose_landmarks,
-                self.mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=self.mp_drawing_styles.get_default_pose_landmarks_style(),
-            )
-
-            # Lista dos landmarks, com seus valores cx, cy e cz
-            lmList = []
-            for lm in results.pose_landmarks.landmark:
-                h, w, _ = img.shape
-                cx, cy, cz = abs(int(lm.x * w)), abs(int(lm.y * h)), (lm.z * 1000)
-                lmList.append(([cx, cy, cz]))
-
-            # Cálculo e exibição da área do contorno formado pelos pontos
-            # Ombro, quadril e joelho: [11, 23, 25, 26, 24, 12]
-            area = findArea(lmList, [11, 23, 24, 12], img)
-
-            # Calculo médio da distância dos landmarks até a camera
-            #dist = estimateDistance(lmList, [11, 12, 23, 24])
-
-            cv2.putText(
-                img,
-                f"Area: {area:.2f}",
-                (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 0),
-                2,
-            )
-
-        rospy.loginfo(f"Area: {area}")
-
-        # cv2.imshow("Pose", cv_img)
-        # cv2.waitKey(1)
-
-        self.pose_detect_img = img
 
     def detect(self, img):
         if self.image_topic == "bebop":
@@ -173,8 +52,8 @@ class Detector:
         else:
             cv_img = img.copy()
 
-        self.hand_detect(cv_img)
-        self.people_detect(cv_img)
+        self.gestureRecognizer(cv_img)
+        self.findArea(cv_img)
 
         out = stackImages(1, [self.hand_detect_img, self.pose_detect_img])
         cv2.imshow("Image", out)
@@ -201,7 +80,7 @@ class Detector:
 
         cap.release()
         cv2.destroyAllWindows()
-    
+
     def run(self):
         print(f"Captura: {self.image_topic}")
 
--- Bebop/src/utils.py
+++ Bebop/src/utils.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env pythonss
+#!/usr/bin/env python
 
 import cv2
 import numpy as np
-
-from cvzone.HandTrackingModule import HandDetector
-from mediapipe.python.solutions.drawing_utils import DrawingSpec
 
 
 def cropImage(img, hcrop, wcrop):
@@ -18,25 +15,6 @@ def cropImage(img, hcrop, wcrop):
     )
 
     return img[height[0] : height[1], width[0] : width[1]]
-
-
-# Calcula a area fromada pela sequencia de pontos
-def findArea(lmList, marks, draw=None):
-    points = np.array(lmList)[marks, :2].astype(int)
-
-    if (draw is not None) and (points.size > 0):
-        cv2.fillPoly(draw, [points], (0, 255, 0))
-
-    return cv2.contourArea(points)
-
-
-# Calcula a média das distâncias cz dos landmarks
-def estimateDistance(lmList, marks):
-    cz = []
-    for i in range(len(marks)):
-        cz.append(lmList[marks[i]][2])
-
-    return np.mean(cz)
 
 
 def drawRectangleEdges(img, x, y, w, h, r):
@@ -104,66 +82,3 @@ def stackImages(scale, imgArray):
         hor = np.hstack(imgArray)
         ver = hor
     return ver
-
-class MyHandDetector(HandDetector):
-    def findHands(self, img, draw=True, flipType=True):
-        """
-        Finds hands in a BGR image.
-        :param img: Image to find the hands in.
-        :param draw: Flag to draw the output on the image.
-        :return: Image with or without drawings
-        """
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
-        allHands = []
-        h, w, c = img.shape
-        if self.results.multi_hand_landmarks:
-            for handType, handLms in zip(
-                self.results.multi_handedness, self.results.multi_hand_landmarks
-            ):
-                myHand = {}
-                ## lmList
-                mylmList = []
-                xList = []
-                yList = []
-                for id, lm in enumerate(handLms.landmark):
-                    px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
-                    mylmList.append([px, py, pz])
-                    xList.append(px)
-                    yList.append(py)
-
-                ## bbox
-                xmin, xmax = min(xList), max(xList)
-                ymin, ymax = min(yList), max(yList)
-                boxW, boxH = xmax - xmin, ymax - ymin
-                bbox = xmin, ymin, boxW, boxH
-                cx, cy = bbox[0] + (bbox[2] // 2), bbox[1] + (bbox[3] // 2)
-
-                myHand["lmList"] = mylmList
-                myHand["bbox"] = bbox
-                myHand["center"] = (cx, cy)
-
-                if flipType:
-                    if handType.classification[0].label == "Right":
-                        myHand["type"] = "Left"
-                    else:
-                        myHand["type"] = "Right"
-                else:
-                    myHand["type"] = handType.classification[0].label
-                allHands.append(myHand)
-
-                ## draw
-                if draw:
-                    landmark_drawing_spec = DrawingSpec(
-                        color=(255, 0, 106), thickness=2, circle_radius=2
-                    )
-                    self.mpDraw.draw_landmarks(
-                        img,
-                        handLms,
-                        self.mpHands.HAND_CONNECTIONS,
-                        landmark_drawing_spec,
-                    )
-        if draw:
-            return allHands, img
-        else:
-            return allHands
