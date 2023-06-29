23c528684ae77f4728b37f9fa3c95de8c321520c
Samuel Lima Braz <115582014+samuellimabraz@users.noreply.github.com>
2023-06-28 22:24:40 -0300

    Update detector.py

--- Bebop/src/detector.py
+++ Bebop/src/detector.py
@@ -6,6 +6,8 @@ from cv_bridge import CvBridge, CvBridgeError
 
 import cv2
 
+import cvzone
+
 from HandModule import MyHandDetector
 from PoseModule import MyPoseDetector
 
@@ -17,13 +19,13 @@ from utils import *
 class Detector(MyHandDetector, MyPoseDetector):
     def __init__(
         self,
-        mode=False,
-        maxHands=2,
-        minHandDetectionCon=0.5,
-        minHandTrackCon=0.5,
-        minFaceDetectionCon=0.5,
-        minPoseDetectionCon=0.5,
-        minPoseTrackCon=0.5,
+        mode,
+        maxHands,
+        minHandDetectionCon,
+        minHandTrackCon,
+        minFaceDetectionCon,
+        minPoseDetectionCon,
+        minPoseTrackCon,
     ):
         MyHandDetector.__init__(
             self,
@@ -35,32 +37,31 @@ class Detector(MyHandDetector, MyPoseDetector):
         )
         MyPoseDetector.__init__(self, mode, True, minPoseDetectionCon, minPoseTrackCon)
 
-        rospy.init_node("detector_node", anonymous=True)
-        self.image_topic = rospy.get_param("~image_topic", "webcam_image")
-
-        self.hand_detect_img = np.zeros((480, 640, 3))
-        self.pose_detect_img = np.zeros((480, 640, 3))
-
         self.bridge = CvBridge()
 
     def detect(self, img):
         if self.image_topic == "bebop":
             try:
-                cv_img = self.bridge.compressed_imgmsg_to_cv2(img, "bgr8")
+                img = self.bridge.imgmsg_to_cv2(img, "bgr8")
             except CvBridgeError as e:
                 print(e)
-        else:
-            cv_img = img.copy()
 
-        self.gestureRecognizer(cv_img)
-        self.findArea(cv_img)
+        # (480, 856, 3)
+        img = cropImage(img, 0.15, 0.3)
 
-        out = stackImages(1, [self.hand_detect_img, self.pose_detect_img])
-        cv2.imshow("Image", out)
-        cv2.waitKey(1)
+        self.findArea(img)
+        self.gestureRecognizer(img)
+
+        #out = cvzone.stackImages([self.hand_detect_img, self.pose_detect_img], 2, 1)
+        
+        cv2.imshow("Detect", img)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            rospy.signal_shutdown("Janela fechada")
+            return
 
     def bebop_run(self):
-        rospy.Subscriber("bebop/image_raw/compressed", CompressedImage, self.detect)
+        rospy.Subscriber("bebop/image_raw", Image, self.detect, queue_size=1)
         rospy.spin()
 
     def webcam_run(self):
@@ -74,14 +75,12 @@ class Detector(MyHandDetector, MyPoseDetector):
 
             self.detect(frame)
 
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                rospy.signal_shutdown("Janela fechada")
-                break
-
         cap.release()
-        cv2.destroyAllWindows()
 
     def run(self):
+        rospy.init_node("detector_node", anonymous=True)
+
+        self.image_topic = rospy.get_param("~image_topic", "webcam_image")
         print(f"Captura: {self.image_topic}")
 
         if self.image_topic == "bebop":
@@ -89,7 +88,23 @@ class Detector(MyHandDetector, MyPoseDetector):
         elif self.image_topic == "webcam":
             self.webcam_run()
 
+def main():
+    opa = Detector(
+        mode=False,
+        maxHands=1,
+        minHandDetectionCon=0.85,
+        minHandTrackCon=0.8,
+        minFaceDetectionCon=0.72,
+        minPoseDetectionCon=0.8,
+        minPoseTrackCon=0.8
+    )
+    opa.run()
+
+    cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
-    opa = Detector()
-    opa.run()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
